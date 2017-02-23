b18da3803d56a2d58a9f213fcb8d52985b70017c
nishaya <nishaya@users.noreply.github.com>
2017-02-24 00:08:37 +0900

    pattern model

--- src/components/seq/fragments/base.jsx
+++ src/components/seq/fragments/base.jsx
@@ -1,7 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component, PropType } from 'react'
 import { Card } from 'antd'
+import Pattern from 'models/pattern'
 
 export default class Base extends Component {
+  static propTypes = {
+    input: PropTypes.instanceOf(Pattern).isRequired,
+  }
+
   render() {
     return(<Card>
       fragment
