183e670eae61d0d83e0b0ae526e12e9485b65fbc
nishaya <nishaya@users.noreply.github.com>
2017-02-25 23:54:28 +0900

    pass modified pattern

--- src/components/seq/fragments/step.jsx
+++ src/components/seq/fragments/step.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Card } from 'antd'
+import { Card, Button } from 'antd'
 import Base from './base'
+import Pattern from 'models/pattern'
 
 export default class Step extends Base {
+  onModified() {
+    const newPattern = new Pattern()
+    Object.assign(newPattern, this.props.pattern)
+    newPattern.foo = 'bar'
+    this.props.onModified(newPattern)
+  }
+
   render() {
     return(<Card>
       step
+      <Button onClick={() => this.onModified()}>test</Button>
     </Card>)
   }
 }
