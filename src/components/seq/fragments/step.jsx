45057061ced168fe95c13080bdefacde7c4d0342
nishaya <nishaya@users.noreply.github.com>
2017-02-27 00:08:30 +0900

    write steps

--- src/components/seq/fragments/step.jsx
+++ src/components/seq/fragments/step.jsx
@@ -16,11 +16,18 @@ export default class Step extends Base {
     }
   }
 
+  checked(index, checked) {
+    const steps = this.state.steps.slice(0)
+    steps[index] = checked
+    super.modifyPattern({ steps })
+    this.setState(steps)
+  }
+
   render() {
     return(<Card title="step">
       <div>
         {this.state.steps.map((step, i) => (
-          (<Checkbox key={`step_${i}`} checked={step} />)
+          (<Checkbox key={`step_${i}`} defaultChecked={step} onClick={e => this.checked(i, e.target.checked)}/>)
         ))}
       </div>
       <Button onClick={() => super.modifyPattern()}>test</Button>
