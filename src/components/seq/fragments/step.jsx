fcf7d5296abdcd7b0837ded442f7bdee05722dda
nishaya <nishaya@users.noreply.github.com>
2017-02-27 00:00:34 +0900

    initialize state

--- src/components/seq/fragments/step.jsx
+++ src/components/seq/fragments/step.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { Card, Button } from 'antd'
+import { Card, Button, Checkbox } from 'antd'
 import Base from './base'
 import Pattern from 'models/pattern'
 
 export default class Step extends Base {
+  constructor(props) {
+    super(props)
+    this.state = {
+      steps: [
+        true, false, false, false,
+        true, false, false, false,
+        true, false, false, false,
+        true, false, false, false,
+      ],
+    }
+  }
+
   render() {
-    return(<Card>
-      step
+    return(<Card title="step">
+      <div>
+        {this.state.steps.map((step, i) => (
+          (<Checkbox key={`step_${i}`} checked={step} />)
+        ))}
+      </div>
       <Button onClick={() => super.modifyPattern()}>test</Button>
     </Card>)
   }
